4f165000a0aec9d12558f9fd3257334cb19392a5
b1endong <tranhoanghai260805@gmail.com>
2025-09-03 01:54:14 +0700

    finish simple  solar system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import {TextureLoader, CubeTextureLoader} from "three";
+import {FlyControls} from "three/addons/controls/FlyControls.js";
 import {useEffect, useRef} from "react";
+import {planets} from "./planets.js";
 
 export default function App() {
     const canvasRef = useRef(null);
@@ -9,19 +12,46 @@ export default function App() {
         const canvas = canvasRef.current;
         const scene = new THREE.Scene();
         const axisHelper = new THREE.AxesHelper(100);
+        const textureLoader = new TextureLoader();
+        const backgroundTextureLoader = new CubeTextureLoader();
 
-        const earthGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-        const earthMaterial = new THREE.MeshBasicMaterial({color: "blue"});
-        const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
-        earthMesh.position.x = 10;
+        backgroundTextureLoader.setPath("src/assets/");
+
+        //Background
+        const backgroundTexture = backgroundTextureLoader.load([
+            "px.png",
+            "nx.png",
+            "py.png",
+            "ny.png",
+            "pz.png",
+            "nz.png",
+        ]);
+        scene.background = backgroundTexture;
+
+        //Saturn Ring with repeating texture
+        const ringGeometry = new THREE.RingGeometry(8, 10, 64); // Tăng segments để smooth hơn
+        const ringTexture = textureLoader.load(
+            `src/assets/2k_saturn_ring_alpha.png`
+        );
+
+        // Thiết lập lặp lại texture theo chiều ngang của vòng
+        ringTexture.repeat.set(16, 3); // Lặp lại 16 lần theo chiều ngang, 3 lần theo bán kính
+        ringTexture.wrapS = THREE.RepeatWrapping;
+        ringTexture.wrapT = THREE.RepeatWrapping;
+
+        const ringMaterial = new THREE.MeshStandardMaterial({
+            map: ringTexture,
+            side: THREE.DoubleSide,
+        });
+
+        const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+        ringMesh.rotation.x = -Math.PI / 2.5;
 
         //Sun
-        const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
-        const sunMaterial = new THREE.MeshBasicMaterial({color: "yellow"});
+        const sunTexture = textureLoader.load(`src/assets/2k_sun.jpg`);
+        const sunMaterial = new THREE.MeshBasicMaterial({map: sunTexture});
+        const sunGeometry = new THREE.SphereGeometry(10, 32, 32);
         const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
-        sunMesh.scale.setScalar(5);
-
-        sunMesh.add(earthMesh);
 
         const camera = new THREE.PerspectiveCamera(
             75,
@@ -33,7 +63,6 @@ export default function App() {
 
         scene.add(sunMesh);
         scene.add(camera);
-        scene.add(axisHelper);
 
         const renderer = new THREE.WebGLRenderer({
             canvas: canvas,
@@ -50,9 +79,68 @@ export default function App() {
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
         const controls = new OrbitControls(camera, canvas);
-        controls.enableDamping = true;
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.002);
+        scene.add(ambientLight);
+        const sunLight = new THREE.PointLight(0xffffff, 1000); // 5 là độ mạnh
+        sunLight.position.set(0, 0, 0);
+        scene.add(sunLight);
+
+        const planetsMeshes = planets.map((planet) => {
+            const planetGeometry = new THREE.SphereGeometry(
+                planet.size * 10,
+                32,
+                32
+            );
+            const planetMesh = new THREE.Mesh(planetGeometry, planet.material);
+            if (planet.name === "saturn") {
+                planetMesh.add(ringMesh);
+            }
+
+            scene.add(planetMesh);
+            planetMesh.position.x = planet.distance;
+            planet.moons.forEach((moon) => {
+                const moonGeometry = new THREE.SphereGeometry(
+                    moon.size * 3,
+                    32,
+                    32
+                );
+                const moonMesh = new THREE.Mesh(moonGeometry, moon.material);
+                planetMesh.add(moonMesh);
+            });
+
+            return planetMesh;
+        });
+
+        console.log(planetsMeshes);
 
         const renderLoop = () => {
+            planetsMeshes.forEach((planet, index) => {
+                planet.rotation.y += planets[index].speed * 0.02 * 2;
+                planet.position.x =
+                    planets[index].distance * Math.sin(planet.rotation.y);
+                planet.position.z =
+                    planets[index].distance * Math.cos(planet.rotation.y);
+                planet.children.forEach((moon, moonIndex) => {
+                    if (
+                        planets[index].moons[moonIndex] &&
+                        planets[index].moons &&
+                        moon.geometry.type != "RingGeometry"
+                    ) {
+                        moon.rotation.y +=
+                            planets[index].moons[moonIndex].speed * 0.01 * 2;
+                        moon.position.x =
+                            planets[index].moons[moonIndex].distance *
+                            Math.sin(moon.rotation.y) *
+                            2;
+                        moon.position.z =
+                            planets[index].moons[moonIndex].distance *
+                            Math.cos(moon.rotation.y) *
+                            2;
+                    }
+                });
+            });
+
             controls.update();
             renderer.render(scene, camera);
             window.requestAnimationFrame(renderLoop);
---
+++ src/planets.js
@@ -0,0 +1,177 @@
+import * as THREE from "three";
+import {TextureLoader} from "three";
+//Textures
+const baseUrl = "src/assets";
+const textureLoader = new TextureLoader();
+const mercuryTexture = textureLoader.load(`${baseUrl}/2k_mercury.jpg`);
+const venusTexture = textureLoader.load(`${baseUrl}/2k_venus.jpg`);
+const earthTexture = textureLoader.load(`${baseUrl}/2k_earth.jpg`);
+const marsTexture = textureLoader.load(`${baseUrl}/2k_mars.jpg`);
+const jupiterTexture = textureLoader.load(`${baseUrl}/2k_jupiter.jpg`);
+const saturnTexture = textureLoader.load(`${baseUrl}/2k_saturn.jpg`);
+const uranusTexture = textureLoader.load(`${baseUrl}/2k_uranus.jpg`);
+const neptuneTexture = textureLoader.load(`${baseUrl}/2k_neptune.jpg`);
+const moonTexture = textureLoader.load(`${baseUrl}/2k_moon.jpg`);
+
+//Materials
+const mercuryMaterial = new THREE.MeshStandardMaterial({
+    map: mercuryTexture,
+});
+const venusMaterial = new THREE.MeshStandardMaterial({
+    map: venusTexture,
+});
+const earthMaterial = new THREE.MeshStandardMaterial({
+    map: earthTexture,
+});
+const marsMaterial = new THREE.MeshStandardMaterial({map: marsTexture});
+const jupiterMaterial = new THREE.MeshStandardMaterial({
+    map: jupiterTexture,
+});
+const saturnMaterial = new THREE.MeshStandardMaterial({
+    map: saturnTexture,
+});
+const uranusMaterial = new THREE.MeshStandardMaterial({
+    map: uranusTexture,
+});
+const neptuneMaterial = new THREE.MeshStandardMaterial({
+    map: neptuneTexture,
+});
+const moonMaterial = new THREE.MeshStandardMaterial({map: moonTexture});
+
+const planets = [
+    {
+        name: "mercury",
+        distance: 15,
+        size: 0.15,
+        material: mercuryMaterial,
+        speed: 0.4,
+        moons: [],
+    },
+    {
+        name: "venus",
+        distance: 30,
+        size: 0.17,
+        material: venusMaterial,
+        speed: 0.25,
+        moons: [],
+    },
+    {
+        name: "earth",
+        distance: 45,
+        size: 0.2,
+        material: earthMaterial,
+        speed: 0.1,
+        moons: [
+            {
+                name: "moon",
+                distance: 2,
+                size: 0.1,
+                material: moonMaterial,
+                speed: 0.2,
+            },
+        ],
+    },
+    {
+        name: "mars",
+        distance: 60,
+        size: 0.49,
+        material: marsMaterial,
+        speed: 0.08,
+        moons: [
+            {
+                name: "phobos",
+                material: moonMaterial,
+                distance: 1.5,
+                size: 0.1,
+                speed: 0.3,
+            },
+            {
+                name: "deimos",
+                material: moonMaterial,
+                distance: 2,
+                size: 0.08,
+                speed: 0.2,
+            },
+        ],
+    },
+    {
+        name: "jupiter",
+        distance: 80,
+        size: 1.3,
+        material: jupiterMaterial,
+        speed: 0.05,
+        moons: [
+            {
+                name: "io",
+                material: moonMaterial,
+                distance: 3,
+                size: 0.29,
+                speed: 0.3,
+            },
+        ],
+    },
+    {
+        name: "saturn",
+        distance: 105,
+        size: 0.6,
+        material: saturnMaterial,
+        speed: 0.03,
+        moons: [
+            {
+                name: "titan",
+                material: moonMaterial,
+                distance: 3,
+                size: 0.4,
+                speed: 0.2,
+            },
+            {
+                name: "enceladus",
+                material: moonMaterial,
+                distance: 2,
+                size: 0.1,
+                speed: 0.25,
+            },
+        ],
+    },
+    {
+        name: "uranus",
+        distance: 130,
+        size: 0.7,
+        material: uranusMaterial,
+        speed: 0.02,
+        moons: [
+            {
+                name: "titania",
+                material: moonMaterial,
+                distance: 2,
+                size: 0.25,
+                speed: 0.2,
+            },
+            {
+                name: "oberon",
+                material: moonMaterial,
+                distance: 3,
+                size: 0.24,
+                speed: 0.18,
+            },
+        ],
+    },
+    {
+        name: "neptune",
+        distance: 155,
+        size: 0.6,
+        material: neptuneMaterial,
+        speed: 0.015,
+        moons: [
+            {
+                name: "triton",
+                material: moonMaterial,
+                distance: 2,
+                size: 0.27,
+                speed: 0.22,
+            },
+        ],
+    },
+];
+
+export {planets};
